5f7d70963ac8d37ead3b97d23a3d59e1ba986fe4
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2025-02-16 23:59:26 +0100

    add sign in

---
+++ app/sign-in.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+
+const SignIn() => {
+  return(
+     <View>
+     <Text>
+     </Text>
+     </View>)
+  ;
+}
+
+export default SignIn;
