d8d3247deb721925b35a3b26da3f534c4278eccc
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2025-03-11 22:20:18 +0100

    work on sign in

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,14 +1,22 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import images from "@/constants/images";
 
 const SignIn = () => {
   return(
-     <View>
-        <Text>
-            SignIn
-        </Text>
-     </View>)
-  ;
+     <SafeAreaView className="bg-white h-full">
+       <ScrollView contentContainerClassName="h-full">
+         <Image source={images.onboarding} className="w-full h-4/6" resizeMode="contain"  />
+
+         <View className="px-10">
+            <Text className="text-base text-center uppercase font-rubik text-black-200">Welcome to RealEstate</Text>
+         </View>
+       </ScrollView>
+     </SafeAreaView>
+     )
+  
 }
 
 export default SignIn;
