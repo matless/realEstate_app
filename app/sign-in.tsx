4de5753ef32db257d3d0a08c74aee91a8ae09e07
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2025-03-12 23:21:08 +0100

    sign in

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -11,7 +11,12 @@ const SignIn = () => {
          <Image source={images.onboarding} className="w-full h-4/6" resizeMode="contain"  />
 
          <View className="px-10">
-            <Text className="text-base text-center uppercase font-rubik text-black-200">Welcome to RealEstate</Text>
+            <Text className="text-base text-center uppercase font-rubik text-black-200">Welcome to Nieru Home</Text>
+
+            <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+               Let's Get You Closer to {"\n"}
+               <Text className="text-primary-300">Your Ideal Home</Text>
+            </Text>
          </View>
        </ScrollView>
      </SafeAreaView>
