40b70325d78c8981d99872ec9c7becdd12ddfd00
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2025-03-16 23:40:10 +0100

    Develop Sign in screen

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import images from "@/constants/images";
+import icons from "@/constants/icons";
 
 const SignIn = () => {
-  return(
+
+   const handleLogin = () => {};
+
+  return (
      <SafeAreaView className="bg-white h-full">
        <ScrollView contentContainerClassName="h-full">
          <Image source={images.onboarding} className="w-full h-4/6" resizeMode="contain"  />
@@ -16,7 +20,25 @@ const SignIn = () => {
             <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
                Let's Get You Closer to {"\n"}
                <Text className="text-primary-300">Your Ideal Home</Text>
-            </Text>
+               </Text>
+
+               <Text className="text-lg font-rubik text-black-200 text-center mt-12">
+                  Login to Nieru Home with google
+               </Text>
+
+               <TouchableOpacity onPress={handleLogin} className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5">
+                  <View className="flex flex-row items-center justify-center ">
+                     <Image
+                  source={icons.google}
+                  className="w-5 h-5"
+                  resizeMode="contain" />
+                  <Text className="text-lg font-rubik-medium text-black-300 ml-2">
+                     Continue with google
+                  </Text>
+                  </View>
+                  
+               </TouchableOpacity>
+            
          </View>
        </ScrollView>
      </SafeAreaView>
