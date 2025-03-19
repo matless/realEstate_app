2e8d8f0da24a1b09089bbbb17895924352477382
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2025-03-19 23:25:31 +0100

    add imports

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,6 +1,18 @@
+import {Account, Avatars, Client} from "react-native-appwrite"
+
 export const config = {
     platform: 'com.NieruHome',
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
 
 }
+
+export const client = new Client();
+
+client
+.setEndpoint(config.endpoint!)
+.setProject(config.projectId!)
+.setPlatform(config.platform!) 
+
+export const avatar = new Avatars(client);
+export const account = new Account(client);
